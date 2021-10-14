0cc6de349938baa89e4c21a920863b8d858805f2
yentencoin <45171910+yentencoin@users.noreply.github.com>
2021-10-14 06:00:01 +0300

    Update deps-linux64.sh

--- deps-linux64/deps-linux64.sh
+++ deps-linux64/deps-linux64.sh
@@ -5,18 +5,18 @@
 # DEPENDS
 
 ## OPENSSL
-wget https://www.openssl.org/source/openssl-1.0.2t.tar.gz
-tar -xvzf openssl-1.0.2t.tar.gz
-cd openssl-1.0.2t/
+wget https://www.openssl.org/source/openssl-1.1.0g.tar.gz
+tar -xvzf openssl-1.1.0g.tar.gz
+cd openssl-1.1.0g/
 ./config no-shared
 make -j$(nproc)
 sudo make install
 cd ..
 
 ## CURL
-wget https://github.com/curl/curl/archive/refs/tags/curl-7_79_1.tar.gz
-tar -xvzf curl-7_79_1.tar.gz
-cd curl-7_79_1/
+wget https://github.com/curl/curl/releases/download/curl-7_57_0/curl-7.57.0.tar.gz
+tar -xvzf curl-7.57.0.tar.gz
+cd curl-7.57.0/
 ./buildconf | grep "buildconf: OK"
 ./configure --disable-shared | grep "Static=yes"
 make -j$(nproc)
